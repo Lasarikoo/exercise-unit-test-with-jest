a3c4456a2eb2d2196ca1a1b1a47ab2fa13e82034
Lasarikoo <lazaro@bega.ltd>
2025-02-17 19:46:57 +0000

    Finished

--- test.js
+++ test.js
@@ -23,3 +23,25 @@ test("One euro should be 1.07 dollars", function() {
     // Hago mi comparación (la prueba)
     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 })
+
+test("One dollar should be 151.39 yen", function() {
+
+    const { fromDollarToYen } = require('./app.js');
+
+    const yens = fromDollarToYen(3.5);
+
+    const expected = 3.5 * 151.39;
+
+    expect(fromDollarToYen(3.5)).toBe(529.865); 
+})
+
+test("One yen should be 0.0052 pounds", function() {
+
+    const { fromYenToPound } = require('./app.js');
+
+    const pounds = fromYenToPound(3.5);
+
+    const expected = 100 * 0.0052;
+
+    expect(fromYenToPound(100)).toBe(0.52); 
+})
